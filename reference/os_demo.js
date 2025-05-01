3f77ab3d1ba8ab85df28c084af2acf989e3126ab
albertombundi <albertombundi@outlook.com>
2025-05-01 23:37:32 +0100

    adicionei mais funções

--- reference/os_demo.js
+++ reference/os_demo.js
@@ -8,3 +8,17 @@ console.log(os.arch());
 
 // CPU core info
 console.log(os.cpus());
+
+
+// Free memory
+console.log(os.freemem());
+
+// Total memory
+console.log(os.totalmem());
+
+
+// Home directory
+console.log(os.homedir());
+
+// Uptime
+console.log(os.uptime());
